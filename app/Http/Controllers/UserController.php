5481ea6d8fc81c47907fb1b7b4e895ad92f13b1e
Rbrt <red.rbrt@hotmail.com>
2016-02-23 00:29:49 -0600

    commit inicial

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Response;
+
+class UserController extends Controller {
+
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index() {
+    return response()->json(User::all());
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id) {
+    return response()->json(User::find($id));
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request) {
+    User::create($request->all());
+    return ['created' => true];
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id) {
+    $user = User::find($id);
+    $user->update($request->all());
+    return ['updated' => true];
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id) {
+    User::destroy($id);
+    return ['deleted' => true];
+  }
+
+}
